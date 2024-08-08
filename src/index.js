e6e9952701a21b5b568e04775405880ea5115192
Flatcher <mslmatth@gmail.com>
2024-08-08 18:03:52 +0200

    installation de react au projet

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.js';
+
+// Create a root.
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the App component to the root.
+root.render(<App />);
