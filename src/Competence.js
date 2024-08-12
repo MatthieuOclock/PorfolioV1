46c954abe60d4857a1ca097de09345636479bbe3
Flatcher <mslmatth@gmail.com>
2024-08-12 19:20:17 +0200

    ajout des infos et structures html. add liste info , compétence et projet attente pour la suite

---
+++ src/Competence.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Competence() { 
+    return (
+        <div className="competence">
+            <hi>Compétences</hi>
+            <ul>
+                <li>Pyhton</li>
+                <li>React</li>
+                <li>Html</li>
+                <li>Css</li>
+                <li>Sass</li>
+                <li>OCaml</li>
+                <li>SQL</li>
+                <li>PostgreSQL</li>
+                <li>Node.js</li>
+                <li>PHP</li>
+                <li>C</li>
+                <li>Shell</li>
+                <li>Git</li>
+                <li>Arduino</li>
+                <li>VHDL</li>
+                <li>Scilab</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Competence; 
---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Header() { 
+    return(
+        <div className="banner">
+            <header>
+                <h1>Matthieu</h1>
+                <h2>ME-HONG-SHIT-LI</h2>
+                <nav>
+                    <ul>
+                        <li>Information</li>
+                        <li>Compétences</li>
+                        <li>Formation</li>
+                        <li>Projets</li>
+                        <li>Expérience Professionnelle</li>
+                        <li>Langues</li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+    )
+} 
+
+export default Header; 
