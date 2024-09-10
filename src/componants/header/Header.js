f89708837f5973c18d491b7351bde71eac017bb1
Flatcher <mslmatth@gmail.com>
2024-09-10 16:29:38 +0200

    amelio page acceuil and header

--- src/componants/header/Header.js
+++ src/componants/header/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './header.css'; 
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
 import Accueil from "../../Accueil";
 import CV from "../../CV";
 import Projets from "../../Projets"; 
@@ -18,14 +18,13 @@ function Header() {
                     </Link>
                     <nav>
                         <ul class="navigation">
-                            <Link to="/" class="page"><li>Acceuil</li></Link>
                             <Link to="/cv" class="page"><li>CV</li></Link>
                             <Link to="/projets" class="page"><li>Projets</li></Link>
                         </ul>
                     </nav>
                 </header>
                 <Routes>
-                    <Route path="/" element={<Accueil />} />
+                    <Route path="*" element={<Accueil />} />
                     <Route path="/cv" element={<CV />} />
                     <Route path="/projets" element={<Projets />} />
                 </Routes>
